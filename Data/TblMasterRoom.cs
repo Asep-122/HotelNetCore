85cfc6a6154e9a6a93ce79ac67ce283197243972
Asep-122 <Asepmunandar122@gmail.com>
2024-09-24 15:55:07 +0700

    update perbaikan

--- Data/TblMasterRoom.cs
+++ Data/TblMasterRoom.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace Hotel.Data
 {
-    public partial class TblMasterRoom d
+    public partial class TblMasterRoom
     {
         public int Id { get; set; }
         public string TipeKamar { get; set; }
